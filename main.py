e97684e894619a534aef7e96f30a4a912a68eb5f
h898ns <weekend5@naver.com>
2024-08-11 16:27:23 +0900

    main.py

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 import os
 import streamlit as st
 
-#import os
-import streamlit as st
-#os.environ["OPENAI_API_KEY"] = " "
+os.environ["OPENAI_API_KEY"] = "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"
 
-#from langchain.llms import OpenAI
-#llm = OpenAI()
+from langchain.llms import OpenAI
+llm = OpenAI()
 
-#content = "방학"
-#result = llm.predict(content + "에 가면 좋을 여행지를 알려줘")
-#print(result)
+content = "방학"
+result = llm.predict(content + "에 가면 좋을 여행지를 알려줘")
+print(result)
 
-from langchain_OpenAI import ChatOpenAI
+from langchain_openai import ChatOpenAI
 
 chatgpt = ChatOpenAI()
 chatgpt = ChatOpenAI(model_name="gpt-3.5-turbo", max_tokens = 512)
 answer = chatgpt.invoke("방학에 가면 좋을 여행지를 알려줘")
-print(answer.content)
+print(answer)
 
 import streamlit as st
 
@@ -26,8 +24,8 @@ import streamlit as st
 #st.title('_streamlit_is :blue[cool] :sunglasses:')
 
 st.set_page_config(
-    page_title="DirChat".
-    page_icon=":bppls:")
+    page_title='DirChat',
+    page_icon=':books:')
 
 st.title("_Private Data :red[QA Chat]_ :books:")
 
@@ -35,7 +33,7 @@ with st.sidebar:
     openai_api_key = st.text_input("OpenAI API key", key="chatbot_api_key",
 type="password:")
     process = st.button("process")
-if process
+if process:
    if not openai_api_key:
     st.info("please add your OpenAI API key to continue.")
     st.stop()
@@ -43,6 +41,7 @@ if process
 content = st.text_input('시의 주제를 제시해 주세요.')
 if st.button('시 작성 요청하기'):
     with st.spinner('시 작성 중....'):
-        result = chatgpt.predict(content + "에 대한 시를 써줘"_
-                                 st.write(result)
+        result = chatgpt.predict(content + "에 대한 시를 써줘")
+        st.write(result)
+            
             
